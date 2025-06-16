97fa919ccf0d1ac6dc10a00bcb051ecb715da00a
Samiksha Mone <samikshamone6@gmail.com>
2025-06-16 22:13:44 +0530

    Create NotificationHub.cs

---
+++ Backend/Controllers/NotificationHub.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace EmployeeTaskManagementSystem.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        // You can extend this for user-specific communication
+    }
+}
